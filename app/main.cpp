d800fc0b9497807a57f032e7ebe0d0b7b3a281a8
kwford <173124447+kwford18@users.noreply.github.com>
2026-07-16 01:17:56 -0700

    Initial commit: Project skeleton structure and CMake config

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Aquarium Engine: Pre-Alpha Starting up..." << std::endl;
+    
+    // TODO: Initialize SDL3 Window & Main Loop
+    
+    return 0;
+}
